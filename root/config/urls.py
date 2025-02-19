f0db144f25ef6a49a818f24e66a43bab3405a8ef
Zase55 <sergiocobo90@hotmail.es>
2025-02-19 13:57:40 +0100

    feat: add initial roots and configs

--- root/config/urls.py
+++ root/config/urls.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from bookings.views import home_request
+from users.views import register, verify_email
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("", home_request, name='home'),
+    path("register/", register, name='register'),
+    path("verify-email/<str:uidb64>/<str:token>/", verify_email, name="verify-email"),
 ]
