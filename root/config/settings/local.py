217f854e6f730e55a7ab4e363a6093cd6ad3db22
Zase55 <sergiocobo90@hotmail.es>
2025-02-28 14:27:52 +0100

    refactor: update path and fix errors

--- root/config/settings/local.py
+++ root/config/settings/local.py
@@ -26,7 +26,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "payments.apps.PaymentsConfig",
     "bookings",
     "cinema",
     "payments",
