35f67492927017c145164894116d0ed7b7b52ed3
Zase55 <sergiocobo90@hotmail.es>
2025-03-04 13:11:55 +0100

    feat: create product model

--- root/payments/models.py
+++ root/payments/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    currency = models.CharField(max_length=7)
+    price = models.FloatField()
+    price_id = models.CharField(max_length=100, null=True, blank=True)
+    
+    def __str__(self):
+        return self.name
